0c3a20ffa8f47427bc41b35c13f083d6ed26e2f6
AHora2 <AHora2@SLB-BZ67JC2.DIR.slb.com>
2016-11-07 17:46:05 +0530

    Adding EurekaServer

---
+++ EurekaServer/src/main/java/com/ash/eurekapoc/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.ash.eurekapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
